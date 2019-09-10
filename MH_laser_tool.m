46301c246b654acbe077ec3a5294a2ba560807a4
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2019-09-10 20:38:39 +0100

    Add GA set TSP solver

--- MH_laser_tool.m
+++ MH_laser_tool.m
@@ -3,7 +3,7 @@ clear
 close all
 
 % import a image file and convert to a .wav for laser display
-image = imread('MH_test.jpg');
+image = imread('MH_test3.jpg');
 %image = imread('star.jpg');
 %image = imread('star2.png');
 %image = imread('cloud2.png');
@@ -42,7 +42,6 @@ while index < size(C,2)
     i = i + 1;
     index = index + C(2,index) + 1;
 end
-%contour(2:end) = contour(1);
 
 max_size = max(size(image,1),size(image,2));
 for n = 1:numel(contour)
@@ -56,10 +55,13 @@ for n = 1:numel(contour)
     % caculate the length, maybe also throw out by lenght, this is sort of
     % draw time i guess
     contour(n).length = sum(sqrt(diff(contour(n).x).^2 + diff(contour(n).y).^2));
+    contour(n).size = numel(contour(n).x);
     
     % start and end points of the contour
     contour(n).start = [contour(n).x(1),contour(n).y(1)];
     contour(n).end = [contour(n).x(end),contour(n).y(end)];
+    
+    contour(n).closed = all(contour(n).start == contour(n).end);
 end
 
 % remove any that have short lenght
@@ -73,76 +75,38 @@ xlim([-1,1])
 ylim([-1,1])
 axis equal
 for n = 1:numel(contour)
-    plot(contour(n).x,contour(n).y,'*-')
+    %plot(contour(n).x,contour(n).y,'-')
+    
+    if contour(n).closed
+        plot(contour(n).x,contour(n).y,'r-*')
+    else
+        plot(contour(n).x,contour(n).y,'b-*')
+    end
 end
 
-
-% conect the contors using greedy algotithum
-% somthing more fancy here could do a better job
-% also this also only considers joining up from the start and end
-% could be some improment to be had by backtracing, or joining part way??
-
-% start at with the longest
-[ ~, index] = max([contour.length]);
-uesed = false(numel(contour),1);
-sorted = zeros(numel(contour),1);
-% find the diatance from this contors end to the start of all contours    
-starts = cell2mat({contour.start}');
-for n = 1:numel(contour)
-    
-    uesed(index) = true;
-    sorted(n) = index;
-            
-    distance = inf(numel(contour),1);
-    distance(~uesed) = sqrt(sum((starts(~uesed) - contour(index).end).^2,2));
-    
-    [ ~, index] = min(distance);
+if any(~[contour.closed])
+    warning('Not all contours are closed')
 end
 
 % two-opt tsp
-[sorted, reversed] = two_opt(starts,cell2mat({contour.end}'),cell2mat({contour.length}'));
+[sorted, reversed] = two_opt(cell2mat({contour.start}'),cell2mat({contour.end}'),cell2mat({contour.length}'));
 
-
-% make sure we have used all the contors, but only onece each
-if ~all(uesed) || numel(unique(sorted)) ~= numel(sorted)
-    error('sorting cock up')
+if all([contour.closed])
+   % use GA to optimise set-TSP
+   [sorted, contour] = GA_set_TSP(contour,sorted);
+   reversed = zeros(numel(contour),1);
 end
 
-% see if we can move the start or end point round
-% closed = true(numel(contour),1);
-% for i = 2:numel(contour)-1
-%     if all(contour(i).start == contour(i).end) 
-%         % this is a closed contor
-%         if ~reversed(i-1)
-%            prev_point =  contour(i-1).end;
-%         else
-%            prev_point =  contour(i-1).start;
-%         end
-%         
-%         if ~reversed(i+1)
-%            next_point =  contour(i+1).start;
-%         else
-%            next_point =  contour(i+1).end;
-%         end
-%         
-%         % find the min distange
-%         length_contour = numel(contour(i).x);
-%         dist = zeros(numel(contour(i).x),1);
-%         for j = 1:length_contour
-%             dist(j) = sqrt((contour(i).x(j) - prev_point(1))^2 + (contour(i).y(j) - prev_point(2))^2) + ...
-%                       sqrt((contour(i).x(j) - next_point(1))^2 + (contour(i).y(j) - next_point(2))^2);
-%         end
-%         
-%         [~,index] = min(dist);        
-%         contour(i).x = contour(i).x([index:length_contour-1,1:index]);    
-%         contour(i).y = contour(i).y([index:length_contour-1,1:index]);
-%     end
-% end
+% make sure we have used all the contors, but only onece each
+if numel(unique(sorted)) ~= numel(sorted) || numel(sorted) ~= numel(contour)
+    error('sorting cock up')
+end
 
 % convert into single X Y data set
 x_data = zeros(numel([contour.x]),1);
 y_data = zeros(numel([contour.x]),1);
 index = 1;
+line_disp = true(sum([contour.size]),1);
 for n = 1:numel(contour)
     contour_size = numel([contour(sorted(n)).x]);
     
@@ -154,18 +118,26 @@ for n = 1:numel(contour)
         y_data(index+(0:contour_size-1)) = fliplr(contour(sorted(n)).y);
     end
     
-    index = index + contour_size;
+    if n ~= numel(contour)
+        index = index + contour_size;
+        line_disp(index-1,1) = false;
+    end
 end
-% make joined up
-x_data(end+1) = x_data(1);
-y_data(end+1) = y_data(1);
 
-% work out the total cost
-cost = sum(sqrt(diff(x_data).^2 + diff(y_data).^2))
+% work out the total cost to check tsp maths
+%cost = sum(sqrt(diff([x_data;x_data(1)]).^2 + diff([y_data;y_data(1)]).^2))
+
+% resample to give more points on display bit
+line_length = sqrt(diff(x_data).^2 + diff(y_data).^2);
+time_step = min(line_length)/1;
+line_length(~line_disp) = min(line_length);
+time = [0;cumsum(line_length)];
+resamp_time = 0:time_step:time(end);
+x_data = interp1(time,x_data,resamp_time');
+y_data = interp1(time,y_data,resamp_time');
 
 % plot joined up contors
 figure
-%subplot(6,4,[1,2])
 subplot(2,4,[1,2])
 
 hold all
@@ -173,7 +145,6 @@ plot(x_data)
 ylabel('X data')
 xlim([1,numel(x_data)])
 
-%subplot(6,4,[5,6])
 subplot(2,4,[5,6])
 
 hold all
@@ -181,10 +152,9 @@ plot(y_data)
 ylabel('Y data')
 xlim([1,numel(x_data)])
 
-%subplot(6,4,[3,4,7,8])
 subplot(2,4,[3,4,7,8])
 hold all
-plot(x_data,y_data)
+plot([x_data;x_data(1)],[y_data;y_data(1)],'*-')
 xlim([-1,1])
 ylim([-1,1])
 axis equal
@@ -194,109 +164,13 @@ title('Single line')
 plot_time = 1/2;
 
 %Sampling Frequency
-Fs = 192000; %1/(plot_time/numel(x_data));
-
-%{
-% % do a low pass of 300hz
-% cutoff_freq = 300;
-% 
-% low_passed = zeros(length(x_data),2);
-% for i = 2:length(x_data)-1
-%     sample = [x_data(i),y_data(i)];
-%     
-%     rc = 1.0/(2*pi*cutoff_freq);
-%     
-%     alpha = (1/Fs)/((1/Fs)+rc);
-%     
-%     alpha = max(alpha,0);
-%     alpha = min(alpha,1);
-%     
-%     
-%     low_passed(i,:) = low_passed(i-1,:) + ((sample - low_passed(i-1,:)) * alpha);
-% end
-% 
-% 
-% subplot(6,4,[9,10])
-% hold all
-% plot(low_passed(:,1))
-% ylabel({'low passed';'X data'})
-% xlim([1,numel(low_passed(:,1))])
-% 
-% subplot(6,4,[13,14])
-% hold all
-% plot(low_passed(:,2))  
-% xlabel('samples')
-% ylabel({'low passed';'Y data'})
-% xlim([1,numel(low_passed(:,2))])
-% 
-% subplot(6,4,[11,12,15,16])
-% hold all
-% title(sprintf('%g Hz low pass filtered',cutoff_freq))
-% plot(low_passed(:,1),low_passed(:,2))
-% xlim([-1,1])
-% ylim([-1,1])
-% axis equal   
-% 
-% % change time step for maximum gradiat
-% % do a low pass of 300hz
-% max_change = mean(sqrt(diff(x_data(:,1)).^2  + diff(y_data(:,1)).^2));
-% time_strech = zeros(length(x_data),1);
-% 
-% for i = 2:length(x_data)
-%     change = sqrt((x_data(i)-x_data(i-1)).^2  + (y_data(i)-y_data(i-1)).^2);
-%     
-%     time_step = min((change / max_change),1);
-%     
-%     time_strech(i) =  time_strech(i-1) + time_step;
-%     
-% 
-% end
-% 
-% intep_time(:,1) = 1:0.1:time_strech(end);
-% x_data_resampled = interp1(time_strech,x_data,intep_time);
-% y_data_resampled = interp1(time_strech,y_data,intep_time);
-% 
-% out_hz = 25;
-% 
-% Fs_interp = Fs;%out_hz*numel(intep_time);
-% 
-% 
-% subplot(6,4,[17,18])
-% hold all
-% plot(time_strech,x_data)
-% plot(x_data)
-% ylabel({'time strech';'X data'})
-% xlim(time_strech([1,end]))
-% 
-% subplot(6,4,[21,22])
-% hold all
-% plot(time_strech,y_data) 
-% plot(y_data)
-% xlabel('time')
-% ylabel({'time strech';'Y data'})
-% xlim(time_strech([1,end]))
-% 
-% subplot(6,4,[19,20,23,24])
-% hold all
-% title('time normalised')
-% plot(x_data,y_data)
-% xlim([-1,1])
-% ylim([-1,1])
-% axis equal   
-% 
-% num_repeat = 500;
-% %file_name = sprintf('test_%i',plot_time);
-% file_name = 'test';
-% %audiowrite([file_name,'_orig.wav'],repmat([x_data,y_data,],num_repeat,1),round(Fs))
-% %audiowrite([file_name,'.wav'],repmat([low_passed(:,1),low_passed(:,2)],num_repeat,1),round(Fs))
-% %audiowrite([file_name,'_resampled.wav'],repmat([x_data_resampled,y_data_resampled],num_repeat,1),round(Fs_interp))
-%}
+Fs = 192000; 
 
 num_repeat = 500;
 file_name = 'test';
 
 % basic resample to get correct number of display hz
-disp_hz = 10;
+disp_hz = 60;
 
 % how many samples do we get to draw the whole thing in?
 total_samples = Fs * (1/disp_hz);
@@ -315,11 +189,11 @@ end
 
 audiowrite([file_name,'_resampled2.wav'],repmat([x_resample2,y_resample2],num_repeat,1),round(Fs))
 
-
 function [Contour_tour, reversed] = two_opt(starts, ends, lengths)
 
 points = [starts; ends;];
 points_contor = [(1:size(starts,1))';(1:size(ends,1))'];
+contour_cost = sum(lengths);
 
 tour_length = size(points,1);
 
@@ -345,7 +219,7 @@ for i = 1:tour_length -1
 end
 cost = cost + cost_mat(end,1);
 
-fprintf('Start Cost %g\n',cost)
+fprintf('Start Cost %g\n',cost-contour_cost)
 
 % Greedy Start
 tour = ones(tour_length,1);
@@ -376,7 +250,7 @@ for i = 1:tour_length -1
 end
 cost = cost + cost_mat(tour(end),tour(1));
 
-fprintf('Greedy Cost %g\n',cost)
+fprintf('Greedy Cost %g\n',cost-contour_cost)
 
 max_iter = 1500;
 for iter = 1:max_iter
@@ -418,7 +292,7 @@ if iter == max_iter
     warning('Two-Opt Max itter')
 end
 
-fprintf('Tow-opt Cost %g\n',cost)
+fprintf('Two-opt Cost %g\n',cost-contour_cost)
 
 % sort back into contor order
 tour = reshape(tour,[2,numel(tour)/2])';
@@ -440,27 +314,33 @@ function contour = simplify_contour(contour)
 % hold all
 % title(sprintf('before %i points',numel(contour.x)))
 % plot(contour.x,contour.y,'*-')
+% axis equal
 
-i = 2;
-while i < numel(contour.y) - 1
-    dy = abs(diff(contour.y([i-1,i,i+1])));
-    dx = abs(diff(contour.x([i-1,i,i+1])));
+i = 1;
+while i < numel(contour.y) - 2
+%   scatter(contour.x(i),contour.y(i),'r')
+%   scatter(contour.x(i+1),contour.y(i+1),'g')
+%   scatter(contour.x(i+2),contour.y(i+2),'k')
     
-    ang = atan2d(dy,dx);
-    line_length = sqrt(dy.^2 + dx.^2);
-    change = diff(ang);
+    dy1 = contour.y(i+1) - contour.y(i);
+    dx1 = contour.x(i+1) - contour.x(i);
+    line_length = sqrt(dy1.^2 + dx1.^2);
+
+    ang1 = atan2(dy1,dx1);
     
-    if  change < -180
-        change = change + 360;
-    elseif change > 180
-        change = change - 360;
-    end
+    dy2 = contour.y(i+2) - contour.y(i);
+    dx2 = contour.x(i+2) - contour.x(i);
+    
+    ang2 = atan2(dy2,dx2);
+
+    change = ang1 - ang2;
+    
+    dist = sin(change) * line_length;
     
     
-    
-    if abs(change) < 5 || any(line_length < 0.001)
-        contour.x(i) = [];
-        contour.y(i) = [];
+    if abs(dist) < 0.0005
+        contour.x(i+1) = [];
+        contour.y(i+1) = [];
     else
         i = i + 1;
     end
@@ -470,12 +350,162 @@ end
 % hold all
 % title(sprintf('after %i points',numel(contour.x)))
 % plot(contour.x,contour.y,'*-')
-
+% axis equal
 
 end
 
+function [sorted, contour] = GA_set_TSP(contour,two_opt)
+
+num_contour = numel(contour);
+contour_size = [contour.size] - 1;
+%contour_cost = sum([contour.length]);
+max_stall = 150;
+max_iter = 5000;
+
+pop_size = 1000;
+keep_pop = max(round(pop_size*0.1),1);
+
+% generate intial population
+pop_tour = zeros(pop_size,num_contour);
+for i = 1:pop_size
+    pop_tour(i,:) = two_opt;    
+end
+pop_start_stop = zeros(pop_size,num_contour);
+for i = 1:num_contour
+    pop_start_stop(:,i) = randi([0,contour_size(i)-1],pop_size,1);
+end
+
+% build cost matrix
+num_points = sum(contour_size);
+points = zeros(num_points,3);
+contour_point_start = zeros(1,num_contour);
+index = 1;
+for i = 1:num_contour
+    contour_point_start(i) = index;
+    points(index:index+contour_size(i)-1,[1,2]) = [contour(i).x(1:end-1);contour(i).y(1:end-1)]';
+    points(index:index+contour_size(i)-1,3) = i;
+    
+    index = index + contour_size(i);
+end
+cost_mat = inf(num_points);
+for i = 1:num_points
+   for j = 1:num_points
+      if points(i,3) == points(j,3)
+          %continue
+      end      
+      cost_mat(i,j) = sqrt(sum( ( points(i,[1,2]) - points(j,[1,2]) ).^2 ));
+   end    
+end
 
 
+index_mat = repmat([1:pop_size]',1,num_contour);
+cost_size = size(cost_mat);
 
+% itterate the GA
+cost = zeros(pop_size,1);
+best_cost = inf;
+stall = 0;
+iter = 0;
+while iter < max_iter && stall < max_stall
+    iter = iter + 1;
+    
+    % evaluate the population
+    index = sub2ind(size(pop_start_stop),index_mat,pop_tour);    
+    cost_mat_points = contour_point_start(pop_tour) + pop_start_stop(index);
+    cost_mat_points1 = cost_mat_points(:,[2:end,1]);
+    index = sub2ind(cost_size,cost_mat_points,cost_mat_points1);
+    cost = sum(cost_mat(index),2);
+
+    [~, best_index] = sort(cost);
+    
+    if best_cost == cost(best_index(1))
+        stall = stall + 1;
+    else
+        stall = 0;
+    end
+    best_cost = cost(best_index(1));
+    
+    %fprintf('%g\n',best_cost);
+    
+    % set whole population to the best and mutate
+    for j = 1:pop_size
+        if j == best_index(1)
+            % leave the best unchanged
+            continue
+        end
+        % pick one of the top
+        index = randi(keep_pop);
+        pop_tour(j,:) = pop_tour(best_index(index),:);
+        pop_start_stop(j,:) = pop_start_stop(best_index(index),:);
+
+        % mutate
+        type = randi(6);
+        switch type
+            case 1
+                % flip, like two opt
+                index = sort(randi(num_contour,1,2));                
+                pop_tour(j,index(1):index(2)) = fliplr(pop_tour(j,index(1):index(2)));                
+                
+            case 2
+                % swap two points
+                index = sort(randi(num_contour,1,2));
+                pop_tour(j,index) = pop_tour(j,fliplr(index)); 
+                
+            case 3
+                % slide along
+                index = sort(randi(num_contour,1,2));
+                pop_tour(j,index(1):index(2)) = pop_tour(j,[index(1)+1:index(2),index(1)]);
+                
+                
+            case 4
+                % pick new start stop point for one
+                index = randi(num_contour);
+                pop_start_stop(j,index) = randi([0,contour_size(index)-1]); 
+                
+            case 5
+                % pick new start stop point for two
+                for l = 1:2
+                    index = randi(num_contour);
+                    pop_start_stop(j,index) = randi([0,contour_size(index)-1]); 
+                end
+                
+            case 6
+                % pick new start stop point for three
+                for l = 1:3                    
+                    index = randi(num_contour);
+                    pop_start_stop(j,index) = randi([0,contour_size(index)-1]);                    
+                end
+        end
+        
+        
+    end
+    
+end
+
+if iter == max_iter
+    warning('GA Max itter')
+end
+
+fprintf('GA Cost %g\n',cost(best_index(1)))
+
+
+sorted = pop_tour(best_index(1),:);
+start_stop = pop_start_stop(best_index(1),:)+1;
+
+% update the contors
+for i = 1:num_contour
+    % remove the duplicate point
+    x = contour(i).x(1:end-1);
+    y = contour(i).y(1:end-1);
+    
+    contour(i).x = x([start_stop(i):end,1:start_stop(i)]);
+    contour(i).y = y([start_stop(i):end,1:start_stop(i)]);
+
+    % update start stop
+    contour(i).start = [contour(i).x(1),contour(i).y(1)];
+    contour(i).end = [contour(i).x(end),contour(i).y(end)];    
+end
+
+end
 
     
